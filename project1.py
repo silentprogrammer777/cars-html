7ff4e8d7cd8f945279cac3c6a10be54f331e8063
bakytbek_umar7 <150159157+silentprogrammer777@users.noreply.github.com>
2024-02-24 23:12:10 +0600

    Add files via upload
    p

---
+++ project1.py
@@ -0,0 +1,26 @@
+# name = "Umar"
+# age = 16
+# example = "Hello {one}, you are {two}".format(two = name, one = age) 
+# print (example)
+# example_2 = f"Hello {name}, you are {age *20}"
+# print (example_2)
+# list = [1,2,3,4.5, "word"]
+# print(len(list))
+# list.reverse()
+# list.append("vocabulary") 
+# print(list)
+
+# my_tuple = (1,2,3,1.45,'word', [7,8,9])[2]
+# # my_tupple.append(10)
+# # my_tupple.reverse()
+# print (my_tuple)
+
+# my_list = [1,2,3,4.15,"a word"]
+# print (my_list[-2])
+
+# exercise_list = ['first entry', [123,456,[0,'Hello :)']], 'bye']
+# print (exercise_list [1][2][1])
+
+# test_list = [1,2,3,4,5,6,7,8,9,10]
+# print (test_list[-1:4:-1])
+
